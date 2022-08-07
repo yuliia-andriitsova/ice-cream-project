ce8c6e2e6662db09086c269777b64f8fef09106f
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-08-07 23:38:55 +0300

    modal-open

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 (() => {
   //Modal Window
-  //   const refs = {
-  //     openModalBtn: document.querySelector('[data-modal-open]'),
-  //     closeModalBtn: document.querySelector('[data-modal-close]'),
-  //     modal: document.querySelector('[data-modal]'),
-  //   };
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-  //   refs.openModalBtn.addEventListener('click', toggleModal);
-  //   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  //   function toggleModal() {
-  //     document.body.classList.toggle('modal-open');
-  //     refs.modal.classList.toggle('is-hidden');
-  //   }
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
 
   //Mobile menu v2
   const menuOpenBtn = document.querySelector('[data-menu-open]');
